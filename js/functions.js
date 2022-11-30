ca8c769de93d7dd41e842bc4ac1ee8fd5f2a9749
Gabriel Girardi <gg@MacBook-Pro-de-Gabriel.local>
2022-11-30 18:13:56 -0300

    refactor: 💡 Remodelado layout

--- js/functions.js
+++ js/functions.js
@@ -1,45 +1,22 @@
-// Função automática para trocar idioma do título
+$(document).ready(function () {
+    let btnMenu = $('.menu-lateral');
+    let menu = $('.container-menu');
 
-setInterval( function() {
-
-    const wel = document.querySelector('#welcome-us');
-    const bem = document.querySelector('#bem-vindo-pt');
-
-    if (wel.style.display === "flex") {
-        wel.style.display = "none";
-        bem.style.display = "flex";
-    } else {
-        wel.style.display = "flex";
-        bem.style.display = "none";
-    }
-
-}, 5000);
-
-// Função para abrir o menu lateral
-
-window.onload = function() {
-    const button = document.querySelector(".menu-lateral");
-    let menu = document.querySelector(".container-menu");
-
-    button.addEventListener("click", function() {
-        if (menu.style.opacity === "0") {
-            menu.style.opacity = "1";
-        } else {
-            menu.style.opacity = "0";
-        }
+    btnMenu.on('click', function () {
+        menu.toggle();
     });
 
-    const more = document.querySelector(".more");
-    let languages = document.querySelector(".languages-content");
+    let btnStart = $('#start-btn');
+    let contentProfile = $('.git-block');
 
-    more.onclick = function() {
-        if (languages.style.opacity === "0") {
-            languages.style.opacity = "1";
-        } else {
-            languages.style.opacity = "0";
-        }
-    }
-}
+    btnStart.on('click', function () {
+        contentProfile.addClass('active');
+    });
+
+    $('.presentation').addClass('active');
+});
+
+
 
 
 

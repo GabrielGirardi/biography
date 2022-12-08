0e211f484ffd8d16d177bd5eb124cb7d70a59a43
GabrielGirardi <gabrielgirardi2811@gmail.com>
2022-12-08 18:23:24 -0300

    feat: 🎸 Feito header

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,40 @@
-$(document).ready(function () {
-    let btnMenu = $('.menu-lateral');
-    let menu = $('.container-menu');
+$(document).ready(function() {
+    let searchBar = $('.search-bar');
+    let searchButton = $('.icon-search');
+    let filter = $('.filter-box');
 
-    btnMenu.on('click', function () {
-        menu.toggle();
+    searchButton.click(function() {
+        searchBar.toggleClass('active');
+        filter.toggleClass('active');
+
+        $('header').click(function() {
+            if(searchBar.is(':focus')) {
+                filter.addClass('active');
+            } else {
+                filter.removeClass('active');
+            }
+        });
     });
-
-    let btnStart = $('#start-btn');
-    let contentProfile = $('.git-block');
-
-    btnStart.on('click', function () {
-        contentProfile.addClass('active');
-    });
-
-    $('.presentation').addClass('active');
 });
 
 
 
-
-
-
+function myFunction() {
+      // Declare variables
+      var input, filter, ul, li, a, i, txtValue;
+      input = document.getElementById('search-bar');
+      filter = input.value.toUpperCase();
+      ul = document.getElementById("myUL");
+      li = ul.getElementsByTagName('li');
+    
+      // Loop through all list items, and hide those who don't match the search query
+      for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+        } else {
+          li[i].style.display = "none";
+        }
+      }
+    }

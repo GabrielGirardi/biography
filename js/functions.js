5919e37e06d8f09ee0535228165e85d02d31e3a8
Gabriel Girardi <gg@MacBook-Pro-de-Gabriel.local>
2023-01-13 23:47:52 -0300

    feat: 🎸 Created new reply texts

--- js/functions.js
+++ js/functions.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     setTimeout(function () {
         $('#typing').hide();
         $('#dialog').show();
-    }, 4000);
+    }, 3000);
 
     function change() {
         $('#typing').show();
@@ -15,83 +15,36 @@ $(document).ready(function () {
         setTimeout(function () {
             $('#typing').hide();
             $('#dialog').show();
-        }, 3000);
-
-        function change() {
-            $('#typing').show();
-            $('#dialog').hide();
-
-            setTimeout(function () {
-                $('#typing').hide();
-                $('#dialog').show();
-            }, 3000);
-        }
-
-        $('.op-1').on('click', function () {
-            $(this).attr('disabled', true);
-            dialogBox.innerHTML = '<p>Claro, você pode me conhecer melhor acessando <a href="./about.html">Aqui</a></p>';
-            change();
-        });
-
-        $('.op-2').on('click', function () {
-            $(this).attr('disabled', true);
-            dialogBox.innerHTML = '<p><a href="https://www.w3.org/html/" target="_blank"><img alt="HTML" src="https://img.shields.io/badge/HTML5%20-%23E34F26.svg?logo=html5&logoColor=white"/></a><a href="https://www.w3schools.com/css/" target="_blank"><img alt="CSS" src="https://img.shields.io/badge/Css%20-%231572B6.svg?logo=css3&logoColor=white"/></a><a href="https://www.w3schools.com/js/" target="_blank"><img alt="js" src="https://img.shields.io/badge/-Javascript-yellow?style=plastic&logo=javascript"/></a><a href="https://sass-lang.com" target="_blank"><img alt="sass" src="https://img.shields.io/badge/Scss-%23563D7C.svg?style=flat&logo=sass&logoColor=white"/></a><a href="https://www.php.net/" target="_blank"><img alt="php" src="https://img.shields.io/badge/-php-purple?style=plastic&logo=php"/></a><a href="https://www.java.com" target="_blank"><img alt="java" src="https://img.shields.io/badge/-java-yellow?style=plastic&logo=java"/></a><a href="https://nodejs.org" target="_blank"><img alt="node" src="https://img.shields.io/badge/-node.js-lightgreen?style=plastic&logo=node.js"/></a></p>';
-            change();
-        });
-
-        $('.op-3').on('click', function () {
-            $(this).attr('disabled', true);
-            dialogBox.innerHTML = '<p>teste</p>';
-            change();
-        });
-
-        $('.op-4').on('click', function () {
-            $(this).attr('disabled', true);
-            dialogBox.innerHTML = '<p>teste</p>';
-            change();
-        });
-
-        $('.op-5').on('click', function () {
-            $(this).attr('disabled', true);
-            dialogBox.innerHTML = '<p>teste</p>';
-            change();
-        });
-
-        dialogBox.hide();
-
-        setTimeout(function () {
-            $('#typing').hide();
-            dialogBox.show();
-        }, 3000);
+        }, 2000);
     }
 
     $('.op-1').on('click', function () {
         $(this).attr('disabled', true);
-        dialogBox.innerHTML = '<p>Claro, você pode me conhecer melhor acessando <a href="./about.html">Aqui</a></p>';
+        dialogBox.innerHTML = '<p>Claro! você pode acessar <a class="links" href="./about.html">Aqui</a> e conhecer melhor toda a minha trajetória nessa loucura de desenvolvimento.</p>';
         change();
     });
 
     $('.op-2').on('click', function () {
         $(this).attr('disabled', true);
-        dialogBox.innerHTML = '<p><a href="https://www.w3.org/html/" target="_blank"><img alt="HTML" src="https://img.shields.io/badge/HTML5%20-%23E34F26.svg?logo=html5&logoColor=white"/></a><a href="https://www.w3schools.com/css/" target="_blank"><img alt="CSS" src="https://img.shields.io/badge/Css%20-%231572B6.svg?logo=css3&logoColor=white"/></a><a href="https://www.w3schools.com/js/" target="_blank"><img alt="js" src="https://img.shields.io/badge/-Javascript-yellow?style=plastic&logo=javascript"/></a><a href="https://sass-lang.com" target="_blank"><img alt="sass" src="https://img.shields.io/badge/Scss-%23563D7C.svg?style=flat&logo=sass&logoColor=white"/></a><a href="https://www.php.net/" target="_blank"><img alt="php" src="https://img.shields.io/badge/-php-purple?style=plastic&logo=php"/></a><a href="https://www.java.com" target="_blank"><img alt="java" src="https://img.shields.io/badge/-java-yellow?style=plastic&logo=java"/></a><a href="https://nodejs.org" target="_blank"><img alt="node" src="https://img.shields.io/badge/-node.js-lightgreen?style=plastic&logo=node.js"/></a></p>';
+        dialogBox.innerHTML = '<p class="tech-languages"><a class="language" href="https://www.w3.org/html/" target="_blank"><img alt="HTML" src="./assets/languages/html.png" width="32" height="32"/></a><a class="language" href="https://www.w3schools.com/css/" target="_blank"><img alt="CSS" src="./assets/languages/css.webp" width="32" height="32"/></a><a class="language" href="https://www.w3schools.com/js/" target="_blank"><img class="language" alt="js" src="./assets/languages/javascript.svg" width="32" height="32"/></a><a class="language" href="https://sass-lang.com" target="_blank"><img alt="sass" src="./assets/languages/sass.png" width="32" height="32"/></a><a class="language" href="https://www.php.net/" target="_blank"><img alt="php" src="./assets/languages/php.png" width="32" height="32"/></a><a class="language" href="https://jquery.com" target="_blank"><img alt="Jquery" src="./assets/languages/jquery.png" width="32" height="32"/></a><a class="language" href="https://nodejs.org" target="_blank"><img alt="node" src="./assets/languages/nodejs.png" width="32" height="32"/></a></p>';
         change();
     });
 
     $('.op-3').on('click', function () {
         $(this).attr('disabled', true);
-        dialogBox.innerHTML = '<p>teste</p>';
+        dialogBox.innerHTML = '<p>Atualmente estou trabalhando como Frontend em uma plataforma de e-commerce incrível chamada <a class="links" href="https://www.magazord.com.br">Magazord</></p>';
         change();
     });
 
     $('.op-4').on('click', function () {
         $(this).attr('disabled', true);
-        dialogBox.innerHTML = '<p>teste</p>';
+        dialogBox.innerHTML = '<p>Contato: <img class="" src="" alt="" width="32" height="32" /></p>';
         change();
     });
 
     $('.op-5').on('click', function () {
         $(this).attr('disabled', true);
-        dialogBox.innerHTML = '<p>teste</p>';
+        dialogBox.innerHTML = '<p>Além do grande amor com a tecnologia, sou apaixonado por desenhar, jogar, assistir filmes e ouvir música!</p>';
         change();
     });
 });
